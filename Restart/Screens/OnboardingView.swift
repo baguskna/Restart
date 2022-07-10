c98434aba50f861f6e1e4fa95af22d90508cb335
Bagus Kurnia <baguskurnia1010@gmail.com>
2022-07-10 17:24:21 +0700

    Add OnboardingView and Module circle component

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,14 +12,44 @@ struct OnboardingView: View {
     
     var body: some View {
         VStack(spacing: 20) {
-            Text("Onboarding")
-                .font(.largeTitle)
+            // Header
+            
+            Spacer()
+            
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                
+                Image("character-2")
+                    .resizable()
+                .scaledToFit()
+            }
+            
+            // Center
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            
+            // Footer
+            
+            Spacer()
             
             Button(action: {
                 isOnboardingViewActive = false
             }) {
-                Text("Start")
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                
+                Text("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
     }
 }
