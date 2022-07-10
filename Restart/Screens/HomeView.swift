3ce4fea5900fef20d754a2c89ec2f95c41907ab9
Bagus Kurnia <baguskurnia1010@gmail.com>
2022-07-10 13:38:52 +0700

    Add HomeView done

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -36,6 +36,73 @@ struct HomeView: View {
                     .multilineTextAlignment(.center)
                     .padding(.horizontal, 10)
                 }
+                
+                // Center Section
+                
+                ZStack {
+                    ZStack {
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                Spacer()
+                
+                // Footer Section
+                
+                ZStack {
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    Text("Get started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            
+                            Circle()
+                                .fill(Color.black.opacity(0.15))
+                                .padding(8)
+                            
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = true
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .frame(height: 80, alignment: .center)
+                .padding()
             }
         }
     }
